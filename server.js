d33a7cb50ef08217058cc7f98468f2a88b86b21f
Snehasish Pati <snehashis.2001@gmail.com>
2022-05-25 15:35:40 +0530

    made changes to server side code

--- server.js
+++ server.js
@@ -9,9 +9,6 @@ const port=process.env.PORT || 5000
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
   }
 
 app.get("/userrepositories",(req,res)=>{
